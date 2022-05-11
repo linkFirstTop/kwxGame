0b7b2d84cbf7065712f8733f5f382722c62942bd
rank <rank@qq.com>
2022-05-12 01:34:32 +0800

    修改提示

--- src/services/room/RoomSocketReceive.ts
+++ src/services/room/RoomSocketReceive.ts
@@ -23,7 +23,7 @@ module room {
 				case RoomProtocol.ACK | RoomProtocol.OGID_CLIENT_LIST_ROOM_LEAVEROOM://请求离开房间
 					this.ON_ACK_LEAVE_TAB(byte);
 					break;
-		
+
 				case RoomProtocol.ACK | RoomProtocol.VGID_USER_MANAGED://服务器通知客户端托管
 					this.ON_ACK_GAMEPLAYERTRUST(byte);
 					break;
@@ -34,7 +34,7 @@ module room {
 					this.OGID_ACK_ROOM_UPDATECOIN(byte);
 					break;
 				case RoomProtocol.ACK | RoomProtocol.OGID_CLIENT_LIST_ROOM_CONTINUE_GAME://更新金币
-				    console.log("====继续游戏===")
+					console.log("====继续游戏===")
 					this.OGID_ACK_ROOM_CONTINUE_GAME(byte);
 					break;
 				case RoomProtocol.ACK | RoomProtocol.VGID_GAME_SYNCGAMEDATA://同步游戏
@@ -43,7 +43,7 @@ module room {
 				case RoomProtocol.ACK | RoomProtocol.VGID_GAME_GAMESTATUS://游戏状态广播消息
 					this.VGID_ACK_GAME_GAMESTATUS(byte);
 					break;
-		
+
 				case RoomProtocol.ACK | RoomProtocol.VGID_GAME_SENDCARD://发牌广播消息
 					this.VGID_ACK_GAME_SENDCARD(byte);
 					break;
@@ -68,20 +68,20 @@ module room {
 
 		// 同步游戏
 		private VGID_ACK_GAME_SYNCGAMEDATA(byte: egret.ByteArray): void {
-			
+
 			var body: room.VGSyncGameDataNtc = room.VGSyncGameDataNtc.decode(byte.bytes);
 			game.GameUserList.saveUserListInfo(body.userInfos)
 			GDGame.Msg.ins.dispatchEventWith(room.RoomMessage.ACK_GAMEPLAYERLIST, false, body);
 
 			game.GamePlayData.SaveHandCarsd(body.userInfos);
-			console.log('=====同步游戏======',body);
-			if( Number( body.status ) == 0 ){
+			console.log('=====同步游戏======', body);
+			if (Number(body.status) == 0) {
 				return;
 			}
 
 			if (Global.isContinue) {
 				//ViewManager.ins.switchToGame();
-				
+
 				ViewManager.ins.hideWait();
 				GDGame.Msg.ins.dispatchEventWith(room.RoomMessage.ACK_GAME_CONTINUE);
 				Global.isContinue = false;
@@ -93,7 +93,7 @@ module room {
 		private VGID_ACK_GAME_GAMESTATUS(byte: egret.ByteArray): void {
 			var body: room.VGGameStatusNtc = room.VGGameStatusNtc.decode(byte.bytes);
 			game.RoomInfo.ins.ChangeStatus(Number(body.status), body.second);
-			
+
 			//console.log('游戏状态广播消息', body);
 		}
 
@@ -107,25 +107,25 @@ module room {
 			game.GameUserList.updateUserListInfo(body.userInfos);
 
 			//GDGame.Msg.ins.dispatchEventWith(game.GameMessage.SHOW_DAPIAO_INFO);
-		
+
 			GDGame.Msg.ins.dispatchEvent(new egret.Event(game.GameMessage.START_GET_CARD, true, true, body));
 			console.log('发牌广播消息', body);
 		}
 
 		//打漂通知
-		private VGID_ACK_USER_DAPIAP(byte: egret.ByteArray){
+		private VGID_ACK_USER_DAPIAP(byte: egret.ByteArray) {
 			const body: room.VGUserDapiaoAck = room.VGUserDapiaoAck.decode(byte.bytes);
 			GDGame.Msg.ins.dispatchEvent(new egret.Event(game.GameMessage.VGID_USER_DAPIAO, true, true, body));
-	
+
 			//console.log("===VGID_ACK_USER_DAPIAP=body",body)	
 		}
 
 		//单张发牌器
-		private VGID_SERVICE_MAGICTILES(byte: egret.ByteArray){
+		private VGID_SERVICE_MAGICTILES(byte: egret.ByteArray) {
 			const body: room.MagicTilesAck = room.MagicTilesAck.decode(byte.bytes);
 			GDGame.Msg.ins.dispatchEvent(new egret.Event(game.GameMessage.VGID_SERVICE_MAGICTILES, true, true, body));
-	
-			 console.log("===单张发牌器=======",body)
+
+			console.log("===单张发牌器=======", body)
 		}
 
 		//行牌单播消息
@@ -133,25 +133,25 @@ module room {
 			const body: room.VGGameOperationNtc = room.VGGameOperationNtc.decode(byte.bytes);
 			game.GamePlayData.SaveChiPengGangHu(body);
 			GDGame.Msg.ins.dispatchEvent(new egret.Event(game.GameMessage.VGID_GAME_OPERATION, true, true, body));
-	
+
 			// console.log('行牌单播消息', body);
 		}
 
 
-	    private	VGID_USER_OPERATION(byte: egret.ByteArray): void {
+		private VGID_USER_OPERATION(byte: egret.ByteArray): void {
 			const body: room.VGUserOperationAck = room.VGUserOperationAck.decode(byte.bytes);
 
 			GDGame.Msg.ins.dispatchEvent(new egret.Event(game.GameMessage.VGID_USER_OPERATION, true, true, body));
-	
+
 		}
 
 		//服务器通知客户端托管
 		private ON_ACK_GAMEPLAYERTRUST(byte: egret.ByteArray): void {
 			//Global.log("服务器通知客户端托管");
 			var body: room.VGUserManagedAck = room.VGUserManagedAck.decode(byte.bytes);
-			Global.log("服务器通知客户端托管",body);
+			Global.log("服务器通知客户端托管", body);
 
-		//	console.log("托管玩家座位号:" + body.TrustSit, "我的座位号:" + Global.userSit);
+			//	console.log("托管玩家座位号:" + body.TrustSit, "我的座位号:" + Global.userSit);
 			GDGame.Msg.ins.dispatchEvent(new egret.Event(game.GameMessage.ACK_GAMEPLAYERTRUST, true, true, body));
 		}
 
@@ -168,9 +168,6 @@ module room {
 		//返回进入游戏服务结果
 		private ON_ACK_LOGIN(byte: egret.ByteArray): void {
 			var body: room.VGLoginAck = room.VGLoginAck.decode(byte.bytes);
-			console.log("===xxxx==========")
-			console.log("====收到登陆返回:" , body);
-			console.log("===xxxx==========")
 			//Global.log("收到登陆返回:" + body.userInfo);
 			if (body.result == 0) {
 				room.RoomWebSocket.instance().roomSender.REQ_ROOMLIST();
@@ -182,7 +179,7 @@ module room {
 				Global.myPos.seatID = body.userInfo.userPos.seatID;
 				Global.userSit = body.userInfo.userPos.seatID;
 				GDGame.Msg.ins.dispatchEvent(new egret.Event(RoomMessage.OGID_ROOM_UPDATECOIN));
-				if (body.userInfo.userPos.seatID != 0){
+				if (body.userInfo.userPos.seatID != 0) {
 					ViewManager.ins.showWait("正在进入未完成牌局的房间，请等待...");
 					Global.isContinue = true;
 				}
@@ -190,6 +187,42 @@ module room {
 					window["sendUserInfo"](body.userInfo.userName, Global.gameID, Global.loadTime);
 				} catch (error) {
 				}
+			} else if (body.result == 1) {
+
+			} else if (body.result == 2) {
+
+			} else if (body.result == 4) {
+
+			} else if (body.result == 21) {
+
+			} else if (body.result == 22) {
+
+			} else if (body.result == 23) {
+				ViewManager.ins.showAlert(Global.dic["账号不存在"]);
+			} else if (body.result == 24) {
+
+			} else if (body.result == 25) {
+				ViewManager.ins.showAlert(Global.dic["账号不存在"]);
+			} else if (body.result == 26) {
+				ViewManager.ins.showAlert(Global.dic["账号不存在"]);
+			} else if (body.result == 401) {
+				ViewManager.ins.showAlert(Global.dic["游戏未开放"]);
+			} else if (body.result == 203) {
+				ViewManager.ins.showAlert(Global.dic["用户状态异常"]);
+			} else if (body.result == 301) {
+				ViewManager.ins.showAlert(Global.dic["账号失效"], function () {
+					window.location.href = location.href;
+				});
+			} else if (body.result == 501) {
+				ViewManager.ins.showAlert(Global.dic["登录失败"]);
+			} else if (body.result == 302) {
+				ViewManager.ins.showAlert(Global.dic["用户频繁登陆"]);
+			} else if (body.result == 305) {
+				ViewManager.ins.showAlert(Global.dic["用户状态异常"]);
+			} else {
+				ViewManager.ins.showAlert(Global.dic["账号不存在"], function () {
+					window.location.href = location.href;
+				});
 			}
 			// if (body.result == 0) {//登陆成功
 			// 	room.RoomWebSocket.instance().roomSender.REQ_ROOMLIST();
@@ -248,7 +281,7 @@ module room {
 			GDGame.Msg.ins.dispatchEvent(new egret.Event(RoomMessage.ON_GAME_LIST, true, true, body.roomInfo));
 			//断线重连进来,不用主动连接，等服务器推送接受链接服务器
 			if (Global.myPos.tableId) {
-	
+
 				let data = new room.VGSitDownReq({
 					userName: Global.userName,
 					userPos: {
@@ -257,10 +290,10 @@ module room {
 						seatID: Global.myPos.seatID
 					}
 				});
-		
+
 				let body = room.VGSitDownReq.encode(data).finish();
 				room.RoomWebSocket.instance().SendMeseage(RoomProtocol.REQ | RoomProtocol.VGID_ROOM_SITDOWN, body);
-			
+
 
 			} else {
 				ViewManager.ins.hideWait();
@@ -282,7 +315,7 @@ module room {
 		//请求进入房间的返回
 		private ON_ACK_ROOM_ENTERROOM(byte: egret.ByteArray): void {
 			var body: room.VGSitDownAck = room.VGSitDownAck.decode(byte.bytes);
-			console.log("进入房间返回结果-----" , body);
+			console.log("进入房间返回结果-----", body);
 			if (body.result == 0) {
 				//请求开始游戏
 				Global.myPos.roomID = body.userInfo.userPos.roomID;
@@ -324,13 +357,13 @@ module room {
 			game.GameParmes.initData();
 
 			// Global.myPos.tableGuid = body.roundGuid;
-		  
+
 			game.GameUserList.saveUserListInfo(body.userInfos);
 			game.GamePlayData.SaveHandCarsd(body.userInfos);
 
 			GDGame.Msg.ins.dispatchEvent(new egret.Event(room.RoomMessage.ACK_ENTER_TABLE, true, true, body));
 
-	
+
 			//断线重联
 		}
 
@@ -340,7 +373,7 @@ module room {
 		}
 		private OGID_ACK_ROOM_CONTINUE_GAME(byte: egret.ByteArray): void {
 			// var body: room.OGContinueGameAck = room.OGContinueGameAck.decode(byte.bytes);
-			 Global.log("收到断线续完返回:" );
+			Global.log("收到断线续完返回:");
 			// if (body.result != 0) {
 			// 	ViewManager.ins.hideWait();
 			// }
